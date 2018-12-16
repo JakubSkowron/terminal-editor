6579fa9cbc9baf11d16e0073b63791f10b6aa6fe
Jakub Skowron <jskowron183@gmail.com>
2018-12-16 02:33:36 +0100

    unicode_hacking: ::wcwidth character_by_character
    Signed-off-by: Jakub Skowron <jskowron183@gmail.com>

--- unicode_hacking/wcswidth.cpp
+++ unicode_hacking/wcswidth.cpp
@@ -14,25 +14,44 @@ void diagnose_string(const char* s) {
   std::size_t nchars = std::mbstowcs(nullptr, s, 0);
   auto wstring = new wchar_t[nchars + 1];
   (void)std::mbstowcs(wstring, s, nchars);
-  std::printf("number or wide chars (std::mbstowcs) = %zd\n", nchars);
 
+  std::printf("number or wide chars = %zd\n", nchars);
   int width = ::wcswidth(wstring, nchars);
   std::printf("number of columns (::wcswidth) = %d\n", width);
 
-  delete wstring;
+  delete[] wstring;
 }
 
 void diagnose_character_by_character(const char* s) {
-  std::printf("::wcwidth(\u25cc\u0302) == %d", ::wcwidth(*L"\u0302"));
+  std::mbtowc(nullptr, nullptr, 0);  // reset state
+
+  // iterate over s
+  const char* p = s;
+  wchar_t wc_buf;
+  while (true) {
+    // MB_CUR_MAX - maximum length of a multibyte character in the current locale
+    int n = std::mbtowc(&wc_buf, p, MB_CUR_MAX);
+    if (n == 0) break;  // end of string
+    if (n == -1) {
+      std::printf("byte: 0x%02x\n", *p);
+      p += 1;
+      continue;  // try next byte
+    }
+
+    std::fputs("character: ", stdout);
+    const char* character_end = p + n;
+    while (p != character_end) {
+      std::fputc(*p++, stdout);
+    }
+    std::printf(", ::wcwidth = %d\n", ::wcwidth(wc_buf));
+  }
 }
 
 int main(int argc, char** argv) {
-  if (!argv[1]) {
-    std::puts("needs a parameter");
-    return 0;
-  }
+  const char* s = argv[1] ? argv[1] : u8"véd [b̪̆e̝ːˀð̠˕ˠ]";
+
   std::setlocale(LC_ALL, "en_US.UTF8");
 
-  char* s = argv[1];
   diagnose_string(s);
+  diagnose_character_by_character(s);
 }
