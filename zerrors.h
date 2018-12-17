88b6aea2a5ab42581125edb8161ffdee50aba2e9
Zbigniew Skowron <zbychs@gmail.com>
2018-12-17 04:32:41 +0100

    Added zlogging.h and zpreprocessor.h.

--- zerrors.h
+++ zerrors.h
@@ -3,6 +3,8 @@
 
 #pragma once
 
+#include "zpreprocessor.h"
+
 #include <iostream>
 #include <string>
 #include <sstream>
@@ -10,15 +12,6 @@
 
 /// Prevents compiler warnings about "unreferenced variable".
 #define ZUNUSED(x) (void)(x)
-
-/// ZTOKEN_STRINGIZE expands to string containing argument.
-#define ZTOKEN_STRINGIZE(x) ZTOKEN_STRINGIZE2(x)
-#define ZTOKEN_STRINGIZE2(x) #x
-
-/// ZVA_OPT_SUPPORTED expands to 'true' if __VA_OPT__ is supported by the compiler. To 'false' otherwise.
-#define ZPP_THIRD_ARG(a,b,c,...) c
-#define ZVA_OPT_SUPPORTED_I(...) ZPP_THIRD_ARG(__VA_OPT__(,),true,false,)
-#define ZVA_OPT_SUPPORTED ZVA_OPT_SUPPORTED_I(?)
 
 namespace terminal_editor {
 
@@ -120,7 +113,7 @@ public:
         message << messageBase;
     }
 
-    [[noreturn]] ~AssertHelper() noexcept(false) {
+    [[noreturn]] ~AssertHelper() {
         auto messageStr = message.str();
         std::cerr << messageStr << std::endl;
         std::abort();
